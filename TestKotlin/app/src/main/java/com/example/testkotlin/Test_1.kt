b1ea65b7083c19d3a6b0d268ac25a9a05022982e
정우 <51356702+jungwo12@users.noreply.github.com>
2021-11-13 23:40:26 +0900

    study1

---
+++ TestKotlin/app/src/main/java/com/example/testkotlin/Test_1.kt
@@ -0,0 +1,18 @@
+package com.example.testkotlin
+
+fun main(){
+    helloWorld()
+
+
+    println(add(4,5))
+}
+
+fun helloWorld(){
+    print("HelloWorld")
+}
+
+fun add(a : Int, b : Int) : Int{
+    return a+b
+}
+
+
